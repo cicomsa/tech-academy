68b91999e4b0476ddd2042ffdb9ead53853c4d4d
Vittorio Guerriero <vittorio.guerriero@cazoo.co.uk>
2020-12-04 10:17:08 +0000

    improve tests description

--- fizzBuzz.test.ts
+++ fizzBuzz.test.ts
@@ -16,22 +16,22 @@ const fizzBuzz = (value: number) : string => {
 
 
 describe('fizzBuzz', () => {
-    it.each([3, 6])('should return "Fizz" if modular of 3 is 0', (value: number) => {
+    it.each([3, 6])('should return "Fizz" when the value is a multiple of 3', (value: number) => {
         const result: string = 'Fizz'
         expect(fizzBuzz(value)).toBe(result);
     });
 
-    it.each([5, 10])('should return "Fizz" if modular of 5 is 0', (value: number) => {
+    it.each([5, 10])('should return "Buzz" when the value is a multiple of 5', (value: number) => {
         const result: string = 'Buzz'
         expect(fizzBuzz(value)).toBe(result);
     });
 
-    it.each([15, 30])('should return "Fizz" if modular of 15 is 0', (value: number) => {
+    it.each([15, 30])('should return "FizzBuzz" when the value is a multiple of 15', (value: number) => {
         const result: string = 'FizzBuzz'
         expect(fizzBuzz(value)).toBe(result);
     });
 
-    it.each([1, 2])('should return %p if modular of 3 or 5 is not 0', (value: number) => {
+    it.each([1, 2])('should return %p when the value is NOT a multiple of 3, 5 or 15', (value: number) => {
         const result: string = `${value}`
         expect(fizzBuzz(value)).toBe(result);
     });
