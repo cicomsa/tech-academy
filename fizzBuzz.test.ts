f2a649d91524fb4ece4f39007f8274c337b57723
Vittorio Guerriero <vittorio.guerriero@cazoo.co.uk>
2020-12-04 10:01:30 +0000

    add test for number 5

--- fizzBuzz.test.ts
+++ fizzBuzz.test.ts
@@ -1,4 +1,7 @@
 const fizzBuzz = (number: number) : string => {
+    if (number === 5) {
+        return "Buzz";
+    }
     return 'Fizz'
 }
 
@@ -7,6 +10,12 @@ describe('fizzBuzz', () => {
     it('should return "Fizz" if number is 3', () => {
         const number: number = 3;
         const result: string = 'Fizz'
+        expect(fizzBuzz(number)).toBe(result);
+    });
+
+    it('should return "Fizz" if number is 5', () => {
+        const number: number = 5;
+        const result: string = 'Buzz'
         expect(fizzBuzz(number)).toBe(result);
     });
 });
